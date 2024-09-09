e4b83b6330cc932db177a8b498c39b66c7ac0dbd
luluferreira13 <ra24204696821@acaonsfatima.org.br>
2024-09-09 11:31:40 -0300

    first commit

---
+++ SRC/rotas.js
@@ -0,0 +1,6 @@
+import e from 'cors'
+import listaNegraController from './controller/listaNegraController.js'
+
+export default function adicionarRotas(servidor){
+    servidor.use(listaNegraController);
+}
